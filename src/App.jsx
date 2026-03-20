371fc85a9fc82b4fb9bb8c49eb14a21bd5697145
Seraphin Eyala <eyalas472@gmail.com>
2026-03-20 02:23:33 +0100

    feat: première requête API TMDB fonctionnelle

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,17 @@
+import { useState, useEffect } from 'react'
+import api from './services/tmdb'
+
 function App() {
+  const [films, setFilms] = useState([])
+
+  useEffect(() => {
+    api.get('/movie/popular')
+      .then(response => {
+        console.log(response.data)
+        setFilms(response.data.results)
+      })
+  }, [])
+
   return (
     <div>
       <h1>CinéSearch</h1>
